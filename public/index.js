1db91b7f31861d28387201a219e96f7b692eafc3
Pranay <pranay.das0909@gmail.com>
2022-10-05 20:41:14 +0530

    Add menus and others

---
+++ public/index.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#menu');
+const burger = document.querySelector('#burger');
+
+burger.addEventListener('click', function () {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+})
